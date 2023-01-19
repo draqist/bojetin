a1a1fc17dc17ab52824b88404c3dad4157786e15
Draq <boomjay008@gmail.com>
2023-01-19 07:36:33 +0100

    feat: working on the signup page UI

---
+++ pages/auth/register.tsx
@@ -0,0 +1,13 @@
+import { Box, Text } from "@chakra-ui/react";
+import Navbar from "../../components/Navbar";
+
+export default function Register() {
+  return (
+    <>
+      <Navbar/>
+      <Box mt={['', '', '70px', '70px', '90px']}>
+        <Text>Register page</Text>
+      </Box>
+    </>
+  )
+}
