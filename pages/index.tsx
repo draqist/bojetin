3b54f583ca9a981fee2cba915ba09b39b3dd3670
Draq <boomjay008@gmail.com>
2023-01-21 18:23:33 +0100

    feat: email verification

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,40 @@
-import { Box, useTheme } from "@chakra-ui/react";
+import { Box, useTheme, useToast } from "@chakra-ui/react";
+import { sendEmailVerification } from "firebase/auth";
+import { useEffect } from "react";
 import AppLayout from "../components/AppLayout";
+import { auth } from "../utils/firebase";
 
 export default function Home() {
   const { bgcolor, text } = useTheme();
+  const toast = useToast();
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if (!user?.emailVerified) {
+        try {
+          // @ts-ignore
+          sendEmailVerification(user).then(() => {
+            toast({
+              title: "Verification required",
+              description: "Kindly check your email to confirm your account",
+              status: "warning",
+              duration: 3000,
+              isClosable: true,
+              position: "top",
+            });
+          })
+        } catch (error) {
+          toast({
+              title: "Could not verify your email",
+              description: "Kindly check your email to confirm your account",
+              status: "error",
+              duration: 3000,
+              isClosable: true,
+              position: "top",
+            });
+        }
+      }
+    })
+  }, [])
   return (
     <AppLayout>
       <Box h="100" bgColor={bgcolor} color={text}>
